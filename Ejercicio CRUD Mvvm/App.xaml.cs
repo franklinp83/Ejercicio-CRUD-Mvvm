9a8c72299e4eb8bf2b14bf364cfe98f3aede7f87
franklinp83 <61525814+franklinp83@users.noreply.github.com>
2024-12-12 23:16:56 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio CRUD Mvvm/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejercicio_CRUD_Mvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Ejercicio CRUD Mvvm/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejercicio_CRUD_Mvvm
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
